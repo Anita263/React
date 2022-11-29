4b5bd4f76abaa4fc6ddaaf8ce067bab8e7bbbe9b
Анна <112005579+Anita263@users.noreply.github.com>
2022-11-29 17:39:40 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Logo from './component/Header/Logo';
+import Header from './component/Header/Header';
+import Item from './component/Main/Item';
+import Main from './component/Main/Main';
+import Logo1 from './component/Footer/Logo1';
+import Footer from './component/Footer/Footer';
+
+import './styles/style.css'
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Main />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
